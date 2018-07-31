eaecdb549b850f41baa2affbe144c128cbe65202
guardianf <578248377@qq.com>
2018-07-31 17:29:37 +0800

    add demos
    增加表单输入绑定的例子：
    1. 增加修饰符的一些例子

--- js/demo7.js
+++ js/demo7.js
@@ -27,4 +27,18 @@ let app2 = new Vue({
         a:'this is a',
         pick:''
     }
+});
+
+let app3 = new Vue({
+    el: '#app-3',
+    data: {
+        msg: 'this is a message',
+        age: 20,
+        msg_2: 'this is a message 2    '
+    },
+    methods: {
+        log(name){
+            console.log(name+':'+this[name])
+        }
+    }
 })
